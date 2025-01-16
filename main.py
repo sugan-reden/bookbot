301db50045cd55d4454af664e6b90ac922affa20
sugan-reden <sugan-reden@pm.me>
2025-01-15 22:18:03 -0500

    Add word counting function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def count_words(message):
+    words = message.split()
+    count = 0
+    for word in words:
+        count += 1
+    return count
+
 def get_book_path(path):
     with open(path) as f:
         return f.read()
