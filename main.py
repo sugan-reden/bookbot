7540b553767207ab73c73f39527660451cf03ba6
sugan-reden <sugan-reden@pm.me>
2025-01-15 22:18:13 -0500

    Add main function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    file_contents = get_book_path(book_path)
+    word_count = count_words(file_contents)
+    print(file_contents)
+    print(f"Word count: {word_count}")
+
 def count_words(message):
     words = message.split()
     count = 0
@@ -9,4 +16,5 @@ def get_book_path(path):
     with open(path) as f:
         return f.read()
 
+main()
 # print("hello world")
