f8de2750d1c3b99b95a4c3cfd6caef8220236ed9
sugan-reden <sugan-reden@pm.me>
2025-01-19 16:17:13 -0500

    Add sorted character count report
    Also some rearranging

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 def main():
     book_path = "books/frankenstein.txt"
+
     file_contents = get_book_path(book_path)
+    # print(file_contents)
 
     word_count = count_words(file_contents)
     # print(f"Word count: {word_count}")
 
     lowered_content = file_contents.lower()
+    # print(lowered_content)
 
     character_counts = check_seen_characters(lowered_content)
     # print(character_counts)
     # print(type(character_counts))
 
-    # print(file_contents)
-    # print(lowered_content)
-    # print("The above content should be all lowercase.")
+    chara_count_list = to_list(character_counts)
+    chara_count_list.sort(reverse=True, key=sort_on)
+    # print(chara_count_list)
 
     # Making a nice report
     print(f"--- Begin report of {book_path} ---")
     print(f"{word_count} words were found in the document")
     print("\n")
-    for i in character_counts:
-        print(f"The '{i}' character was found {character_counts[i]} times")
+    # for i in character_counts:
+    #     print(f"The '{i}' character was found {character_counts[i]} times")
+    for i in chara_count_list:
+        temp_dict = i
+        print(f"The '{temp_dict["character"]}' character was found {temp_dict["count"]} times")
     print("--- End report ---")
+
+def sort_on(dict):
+    return dict["count"]
+
+def to_list(dict):
+    count_list = []
+    for key in dict:
+        count_list.append({"character": key, "count": dict[key]})
+    return count_list
 
 def check_seen_characters(lowered_text):
     seen_characters = {}
